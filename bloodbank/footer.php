2a52dcc7611819917d47e2421dec5dc0a5c82a5b
Manoj S <138807698+manoj635@users.noreply.github.com>
2024-08-09 20:18:30 +0530

    Delete bloodbank directory

--- bloodbank/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer text-center bg-blue">
-	© Copyright <span id="demo"></span> <span class="brand">Blood Bank Management System . </span> All Rights Reserved.
-	<script>
-		date = new Date();
-    	document.getElementById("demo").innerHTML=date.getFullYear();
-    </script>
-</footer>
